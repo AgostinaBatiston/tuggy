3b0c1bbc898d437f4818cf13631af491112d85fd
Agostina Batiston <agbatiston@hotmail.com>
2021-03-17 22:36:40 -0300

    Agregue un objeto

--- js/repositorio-tuggy.js
+++ js/repositorio-tuggy.js
@@ -1,3 +1,31 @@
+class ShoppingCart {
+    constructor() {
+        this.cart = [];
+    }
+    getAdd = function(prod) {
+        this.cart.push(prod);
+    }
+    getFullCart = function() {
+        console.log('Tenés ' + this.cart.length + ' productos en el carrito');
+        for (var i = 0; i < this.cart.length; i++) {
+            console.log(this.cart[i]);
+        }
+    }
+}
+class miProducto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+}
+
+var producto1 = new miProducto('Prod 1', 450);
+var producto2 = new miProducto('Prod 2', 800);
+
+
+
+
 var lista = document.createElement("div");
 lista.classList.add("mi_lista");
 document.body.appendChild(lista);
