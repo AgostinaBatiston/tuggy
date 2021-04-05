ff8abf18a597d54fdf9d63ffcf626f04e67ccfdd
Agostina Batiston <agbatiston@hotmail.com>
2021-04-04 22:33:11 -0300

    agregue un archivo json con los productos

---
+++ js/data.js
@@ -0,0 +1,121 @@
+[{
+    "id": 1,
+    "producto": "Batman y robin",
+    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
+    "precio": 750
+}, {
+    "id": 2,
+    "producto": "Bts",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Llaverito de personajes de bts pintados a mano. Medidas: 5x5cm",
+    "precio": 250
+}, {
+    "id": 3,
+    "producto": "Charmander",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 4,
+    "producto": "Cubone",
+    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 5,
+    "producto": "Edward y Alphonse",
+    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
+    "precio": 750
+}, {
+    "id": 6,
+    "producto": "Flash",
+    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
+    "precio": 430
+}, {
+    "id": 7,
+    "producto": "Gollum",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Peluche mediano. Medidas: 20x13cm",
+    "precio": 650
+}, {
+    "id": 8,
+    "producto": "Harry potter",
+    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 9,
+    "producto": "Iron-man",
+    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 10,
+    "producto": "Hello Kitty",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 11,
+    "producto": "Mazinger",
+    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "detalles": "Peluche mediano. Medidas: 20x13cm",
+    "precio": 329
+}, {
+    "id": 12,
+    "producto": "Gato/a",
+    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "detalles": "LLaverito de gato/a personalizado. Medidas: 6x6cm",
+    "precio": 350
+}, {
+    "id": 13,
+    "producto": "Perro/a",
+    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "detalles": "LLaverito de perro/a personalizado. Medidas: 6x6cm",
+    "precio": 400
+}, {
+    "id": 14,
+    "producto": "Pidgeotto",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 15,
+    "producto": "Pokemones",
+    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "detalles": "LLaveritos de pokemones a elección. Medidas: 7x7,50cm",
+    "precio": 300
+}, {
+    "id": 16,
+    "producto": "Rick",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
+    "precio": 430
+}, {
+    "id": 17,
+    "producto": "Spider-man",
+    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 18,
+    "producto": "Squirtle",
+    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "detalles": "Peluche pequeño. Medidas: 12x7cm",
+    "precio": 400
+}, {
+    "id": 19,
+    "producto": "Taki y Mitsuha",
+    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
+    "precio": 750
+}, {
+    "id": 20,
+    "producto": "Thomas Shelby",
+    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "detalles": "Llavero pequeño. Medidas: 12x7cm",
+    "precio": 430
+}]
