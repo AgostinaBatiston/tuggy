a1cb0ee951b10f9e189d39eaf60bf2f6a6b16ff6
Agostina Batiston <agbatiston@hotmail.com>
2021-04-05 11:15:14 -0300

    Borre el contenido de los productos en tienda.html y los voy a generar con jquery y json

--- js/data.js
+++ js/data.js
@@ -1,121 +1,121 @@
-[{
+const Datos = [{
     "id": 1,
     "producto": "Batman y robin",
-    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "imagen": "./css/imagenes/batman_robin.jpg",
     "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
     "precio": 750
 }, {
     "id": 2,
     "producto": "Bts",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/bts.jpg",
     "detalles": "Llaverito de personajes de bts pintados a mano. Medidas: 5x5cm",
     "precio": 250
 }, {
     "id": 3,
     "producto": "Charmander",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/charmander.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 4,
     "producto": "Cubone",
-    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "imagen": "./css/imagenes/cubone.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 5,
     "producto": "Edward y Alphonse",
-    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "imagen": "./css/imagenes/EdYAl.jpg",
     "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
     "precio": 750
 }, {
     "id": 6,
     "producto": "Flash",
-    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "imagen": "./css/imagenes/flash.jpg",
     "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
     "precio": 430
 }, {
     "id": 7,
     "producto": "Gollum",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/gollum.jpg",
     "detalles": "Peluche mediano. Medidas: 20x13cm",
     "precio": 650
 }, {
     "id": 8,
     "producto": "Harry potter",
-    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "imagen": "./css/imagenes/HP.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 9,
     "producto": "Iron-man",
-    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "imagen": "./css/imagenes/ironman.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 10,
     "producto": "Hello Kitty",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/kitty.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 11,
     "producto": "Mazinger",
-    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "imagen": "./css/imagenes/mazinger.jpg",
     "detalles": "Peluche mediano. Medidas: 20x13cm",
-    "precio": 329
+    "precio": 650
 }, {
     "id": 12,
     "producto": "Gato/a",
-    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "imagen": "./css/imagenes/oggy.jpg",
     "detalles": "LLaverito de gato/a personalizado. Medidas: 6x6cm",
     "precio": 350
 }, {
     "id": 13,
     "producto": "Perro/a",
-    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "imagen": "./css/imagenes/ovejero.jpg",
     "detalles": "LLaverito de perro/a personalizado. Medidas: 6x6cm",
     "precio": 400
 }, {
     "id": 14,
     "producto": "Pidgeotto",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/pidgeotto.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 15,
     "producto": "Pokemones",
-    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "imagen": "./css/imagenes/pokemones.jpg",
     "detalles": "LLaveritos de pokemones a elección. Medidas: 7x7,50cm",
     "precio": 300
 }, {
     "id": 16,
     "producto": "Rick",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/Rick.jpg",
     "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
     "precio": 430
 }, {
     "id": 17,
     "producto": "Spider-man",
-    "imagen": "http://dummyimage.com/250x250.png/cc0000/ffffff",
+    "imagen": "./css/imagenes/spiderman.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 18,
     "producto": "Squirtle",
-    "imagen": "http://dummyimage.com/250x250.png/5fa2dd/ffffff",
+    "imagen": "./css/imagenes/squirtle.jpg",
     "detalles": "Peluche pequeño. Medidas: 12x7cm",
     "precio": 400
 }, {
     "id": 19,
     "producto": "Taki y Mitsuha",
-    "imagen": "http://dummyimage.com/250x250.png/ff4444/ffffff",
+    "imagen": "./css/imagenes/yourName.jpg",
     "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
     "precio": 750
 }, {
     "id": 20,
     "producto": "Thomas Shelby",
-    "imagen": "http://dummyimage.com/250x250.png/dddddd/000000",
+    "imagen": "./css/imagenes/Llavero.jpg",
     "detalles": "Llavero pequeño. Medidas: 12x7cm",
     "precio": 430
 }]
