f8e570c4168098c4c737ffddbcfc1ae6be8ae2b3
Agostina Batiston <agbatiston@hotmail.com>
2021-04-25 21:35:54 -0300

    Le modifique el tamaño a las imagenes de los productos

--- js/repositorio-tuggy.js
+++ js/repositorio-tuggy.js
@@ -7,121 +7,121 @@
         this.catalogo = [{
             "id": 1,
             "producto": "Batman y robin",
-            "imagen": "./css/imagenes/batman_robin.jpg",
+            "imagen": "batman_robin.jpg",
             "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
             "precio": 750
         }, {
             "id": 2,
             "producto": "Bts",
-            "imagen": "./css/imagenes/bts.jpg",
+            "imagen": "bts.jpg",
             "detalles": "Llaverito de personajes de bts pintados a mano. Medidas: 5x5cm",
             "precio": 250
         }, {
             "id": 3,
             "producto": "Charmander",
-            "imagen": "./css/imagenes/charmander.jpg",
+            "imagen": "charmander.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 4,
             "producto": "Cubone",
-            "imagen": "./css/imagenes/cubone.jpg",
+            "imagen": "cubone.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 5,
             "producto": "Edward y Alphonse",
-            "imagen": "./css/imagenes/EdYAl.jpg",
+            "imagen": "EdYAl.jpg",
             "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
             "precio": 750
         }, {
             "id": 6,
             "producto": "Flash",
-            "imagen": "./css/imagenes/flash.jpg",
+            "imagen": "flash.jpg",
             "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
             "precio": 430
         }, {
             "id": 7,
             "producto": "Gollum",
-            "imagen": "./css/imagenes/gollum.jpg",
+            "imagen": "gollum.jpg",
             "detalles": "Peluche mediano. Medidas: 20x13cm",
             "precio": 650
         }, {
             "id": 8,
             "producto": "Harry potter",
-            "imagen": "./css/imagenes/HP.jpg",
+            "imagen": "HP.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 9,
             "producto": "Iron-man",
-            "imagen": "./css/imagenes/ironman.jpg",
+            "imagen": "ironman.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 10,
             "producto": "Hello Kitty",
-            "imagen": "./css/imagenes/kitty.jpg",
+            "imagen": "kitty.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 11,
             "producto": "Mazinger",
-            "imagen": "./css/imagenes/mazinger.jpg",
+            "imagen": "mazinger.jpg",
             "detalles": "Peluche mediano. Medidas: 20x13cm",
             "precio": 650
         }, {
             "id": 12,
             "producto": "Gato/a",
-            "imagen": "./css/imagenes/oggy.jpg",
+            "imagen": "oggy.jpg",
             "detalles": "LLaverito de gato/a personalizado. Medidas: 6x6cm",
             "precio": 350
         }, {
             "id": 13,
             "producto": "Perro/a",
-            "imagen": "./css/imagenes/ovejero.jpg",
+            "imagen": "ovejero.jpg",
             "detalles": "LLaverito de perro/a personalizado. Medidas: 6x6cm",
             "precio": 400
         }, {
             "id": 14,
             "producto": "Pidgeotto",
-            "imagen": "./css/imagenes/pidgeotto.jpg",
+            "imagen": "pidgeotto.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 15,
             "producto": "Pokemones",
-            "imagen": "./css/imagenes/pokemones.jpg",
+            "imagen": "pokemones.jpg",
             "detalles": "LLaveritos de pokemones a elección. Medidas: 7x7,50cm",
             "precio": 300
         }, {
             "id": 16,
             "producto": "Rick",
-            "imagen": "./css/imagenes/Rick.jpg",
+            "imagen": "Rick.jpg",
             "detalles": "Llavero de personaje pequeño. Medidas: 12x7cm",
             "precio": 430
         }, {
             "id": 17,
             "producto": "Spider-man",
-            "imagen": "./css/imagenes/spiderman.jpg",
+            "imagen": "spiderman.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 18,
             "producto": "Squirtle",
-            "imagen": "./css/imagenes/squirtle.jpg",
+            "imagen": "squirtle.jpg",
             "detalles": "Peluche pequeño. Medidas: 12x7cm",
             "precio": 400
         }, {
             "id": 19,
             "producto": "Taki y Mitsuha",
-            "imagen": "./css/imagenes/yourName.jpg",
+            "imagen": "yourName.jpg",
             "detalles": "Peluches pequeños pintados a mano. Medidas: 12x7cm",
             "precio": 750
         }, {
             "id": 20,
             "producto": "Thomas Shelby",
-            "imagen": "./css/imagenes/Llavero.jpg",
+            "imagen": "Llavero.jpg",
             "detalles": "Llavero pequeño. Medidas: 12x7cm",
             "precio": 430
         }];
@@ -179,7 +179,7 @@
                 <div class="column is-one-quarter">
                 <div class="card">
                     <div class="card-image">
-                        <img src="./../css/imagenes/${carrito.catalogo[i].imagen}" alt="Placeholder">
+                        <img src="./css/imagenes/${carrito.catalogo[i].imagen}" class="imagenTienda">
                     </div>
                     <div class="card-content">
                         <h2 class="title is-3">${carrito.catalogo[i].producto}</h2>
